f2c7d2e1c88bef1870863351fe3e7d03fc0e56ac
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-07-17 16:47:30 +0300

    1st try

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+// Task 15
+
+// const num =+prompt('Please enter your number');
+// let isSimple = true;
+//
+// if ( isNaN(num) === true || num === 0 ) {
+//     alert('You entered invalid number. Please try again');
+//
+// } else {
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             isSimple = false;
+//             break;
+//         }
+//     }
+//     if (isSimple && num > 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+//
+// Task 16
+
+const num =+prompt('Please enter your number');
+
+if ( isNaN(num) === true || num === 0 ) {
+    alert('You entered invalid number. Please try again');
+} else if (num <= 1){
+    console.log(NaN);
+} else  {
+    for (let i = 2; i <= num; i++) {
+
+        if (num % i === 0) {
+            console.log(i);
+            break;
+        }
+    }
+}
